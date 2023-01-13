4d422a87eb7dae007e4c1caad8ead05da318c8e6
Greghori101 <e.souala@esi-sba.dz>
2023-01-13 18:04:21 +0100

    first commit

--- conn.php
+++ conn.php
@@ -7,17 +7,17 @@ $mdp = '2rvhj1QXzVPd2mkYHqj7';
 
 try {
     $bdd = new PDO('mysql:host=' . $host . ';dbname=' . $bdd_name . ';charset=utf8', $login, $mdp);
-    $sql = "CREATE TABLE 'components' (
-            'id' int NOT NULL AUTO_INCREMENT,
-            'name' varchar(45) DEFAULT NULL,
-            'image' mediumblob,
-            'purshased_at' date DEFAULT NULL,
-            'quantity' int DEFAULT NULL,
-            'state' varchar(45) DEFAULT NULL,
-            'extension' varchar(45) DEFAULT NULL,
-            PRIMARY KEY ('id')
-          ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-          ";
+    $sql = "CREATE TABLE `components` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `name` varchar(45) DEFAULT NULL,
+        `image` mediumblob,
+        `purshased_at` date DEFAULT NULL,
+        `quantity` int DEFAULT NULL,
+        `state` varchar(45) DEFAULT NULL,
+        `extension` varchar(45) DEFAULT NULL,
+        PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+      ";
     $stmt = $bdd->prepare($sql);
     $stmt->execute();
 } catch (PDOException $e) {
